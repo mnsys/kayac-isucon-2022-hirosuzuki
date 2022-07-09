5172428a6fb001d80e1d09017990049a0b59791c
mynavi-hirosuzuki <suzuki.hiroaki@mynavi.jp>
2022-07-09 13:43:53 +0000

    ADD INDEX: playlist_favorite (fav...

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE INDEX playlist_ulid ON playlist (ulid);
 CREATE INDEX playlist_public_created_at ON playlist (is_public, created_at DESC);
 CREATE INDEX song_ulid ON song (ulid);
+CREATE INDEX playlist_favorite_index02 ON playlist_favorite (favorite_user_account, created_at DESC);
